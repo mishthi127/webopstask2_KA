a8ff3a6988110bb9e8715ef9e981d959580d4ef8
mishthi127 <166861368+mishthi127@users.noreply.github.com>
2025-07-07 23:38:24 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Scans all JS, JSX, TS, TSX files in the 'src' folder.
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["sans-serif"],
+      },
+    },
+  },
+  plugins: [], 
+};
